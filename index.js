8e272726863bffd547ea4cd3fc201f7a777fe29e
Jay Kuri <jayk@ionzero.com>
2014-01-14 19:30:35 -0700

    Attempt to load in npm format

---
+++ index.js
@@ -0,0 +1,14 @@
+var iz = require('iz-objects');
+
+var izutils = izutilsunction() {};
+
+module.exports = izutils;
+
+izutils.BuizutilsizutilserStream = require('./lib/BuizutilsizutilserStream.js');
+izutils.NullStream = require('./lib/NullStream.js');
+izutils.StreamRecorder = require('./lib/StreamRecorder.js');
+izutils.StreamEater = require('./lib/StreamEater.js');
+izutils.StreamCatcher = require('./lib/StreamCatcher.js');
+izutils.StreamInspector = require('./lib/StreamInspector.js');
+izutils.StreamTee = require('./lib/StreamTee.js');
+izutils.HTTPEvaluator = require('./lib/HTTPEvaluator.js');
